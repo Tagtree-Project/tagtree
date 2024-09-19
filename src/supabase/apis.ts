2cc6f610991eb89530accf708411efb0b478ad2a
조윤식 <cho.yunsig56@gmail.com>
2024-09-19 16:29:20 +0900

    환경변수명 변경

--- src/supabase/apis.ts
+++ src/supabase/apis.ts
@@ -3,7 +3,7 @@ import type { Group, Tag, Relation, Markdown } from "./responses";
 
 const supabase = createClient(
   process.env.SUPABASE_URL ?? "",
-  process.env.SUPABASE_KEY ?? ""
+  process.env.SUPABASE_ANON_KEY ?? ""
 );
 
 export async function getAllGroups(): Promise<Group[] | undefined> {
